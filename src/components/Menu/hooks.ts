5e106b2125831afd64babd14dc83848ea6406341
zhangshiqi <q18056090106@163.com>
2023-03-17 18:40:29 +0800

    feat: 菜单方法

--- src/components/Menu/hooks.ts
+++ src/components/Menu/hooks.ts
@@ -1,37 +1,39 @@
-import { getMenuList } from './index'
+import { getMenuList } from "./index";
 
 export async function handleMenuData() {
-  const menuData = await getMenuList()
-  console.log(menuData);
+  const menuData = await getMenuList();
+  console.log(menuData, 'line 5');
 
-  const treeData = {
-    id: '',
-    label: '',
-    children: []
-  }
+  // const treeData = {
+  //   id: '',
+  //   label: '',
+  //   children: []
+  // }
 
-  const menuDataArr: string[] = []
+  const menuDataArr: string[] = [];
 
   for (let i = 0; i <= menuData.length; i++) {
-    if (!menuDataArr.includes(menuData[i].parentAuthName)) {
-      menuDataArr.push(menuData[i].parentAuthName)
+    if (!menuDataArr.includes(menuData[i]?.parentAuthName)) {
+      menuDataArr.push(menuData[i]?.parentAuthName);
     }
   }
 
+  const A = menuDataArr.map((item, index) => {
+    return  menuData.filter((itemMenu: Record<string, any>) => {
+      return itemMenu.parentAuthName === item
+    }).map((itemMenu: Record<string, any>, indexMenu: number) => {
+      if (itemMenu.parentAuthName === item) {
+        return {
+          label: itemMenu.authName,
+          index: index + "-" + indexMenu
+        }
+      }
+    });
+  });
 
-
-
-  // const parentArrData = menuData.filter((item: Record<string, any>, index: number) => {
-  //
-  //
-  //
-  //
-  //
-  //
-  // })
-
+  console.log(A);
 
 }
 
 
-handleMenuData()
+handleMenuData();
